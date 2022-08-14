472dcfa75ec0836dc6a6aceda4018673b63f679a
lijiacheng <1215253962@qq.com>
2022-08-14 10:48:43 +0800

    first java homework

---
+++ chapter2/homework1.java
@@ -0,0 +1,9 @@
+//编写一个hello，world程序
+
+public class homework1 {
+	//编写一个main方法
+	public static void main(String[] args){
+		//编写hello,world程序
+		System.out.println("hello,world");
+	}
+}
---
+++ chapter2/homework2.java
@@ -0,0 +1,7 @@
+//将个人的基本信息（姓名、性别、籍贯、住址）打印到控制台上输出。各条信息分别占一行
+
+public class homework2{
+	public static void main(String[] args) {
+		System.out.println("姓名\t性别\t籍贯\t住址\n李嘉成\t男\t山西临汾\t山西省侯马市");
+	}
+}
